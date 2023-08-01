3e56485d43ce8ffbdf27cc96a56220c43a8a912d
cMardc <130239955+cMardc@users.noreply.github.com>
2023-08-01 17:34:29 +0400

    Create thanks.cpp

---
+++ src/thanks.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+  cout << "Thanks for visiting my github repo, this file is for making github recognizing my project as C++ and not makefile. Thanks!\n";
+}
